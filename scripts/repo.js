f5c1664d14bbaa12703bd16d9b24dadbfcf2fff1
seanloughran <seancloughran@gmail.com>
2016-06-12 14:04:00 -0700

    Token generated

--- scripts/repo.js
+++ scripts/repo.js
@@ -7,11 +7,10 @@
   // to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
     $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
+      url: '/github/users/seanloughran/repos' +
             '?per_page=100' +
             '&sort=updated',
       type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
       success: function(data, message, xhr) {
         repos.all = data;
       }
